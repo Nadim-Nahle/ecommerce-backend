dcc69d2710bb4ea458c1b2bba1331342b5f67c2d
Nadim-Nahle <nahlenadim@gmail.com>
2023-09-27 13:35:54 +0300

    add service account

--- index.ts
+++ index.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import * as functions from 'firebase-functions';
 import { AppModule } from './src/app.module';
-
-let serviceAccount = require('./ecommerce-nadim-firebase-adminsdk-ayo50-040961381b.json');
+import * as admin from 'firebase-admin';
 
 const expressServer = express();
+
+import * as serviceAccount from './key.json'; // Adjust the filename as needed
+
+const serviceAccountKey: admin.ServiceAccount = serviceAccount as admin.ServiceAccount;
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+  databaseURL: 'https://ecommerce-nadim.firebaseio.com',
+});
+
 const createFunction = async (expressInstance): Promise<void> => {
   const app = await NestFactory.create(
     AppModule,
@@ -18,4 +28,3 @@ export const api = functions.https.onRequest(async (request, response) => {
   await createFunction(expressServer);
   expressServer(request, response);
 });
-

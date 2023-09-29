a155036a2f79b879448bcb46cafb13fb198a17a7
Nadim-Nahle <nahlenadim@gmail.com>
2023-09-29 14:12:12 +0300

    Update create product contrioller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get  } from '@nestjs/common';
+import { Controller, Get, Post, Body,UseFilters } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { CreateProductDto } from './create-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,5 +9,10 @@ export class ProductsController {
     @Get()
     async getAllProducts() {
         return this.productsService.getAllProducts();
+    };
+      @Post('add')
+      create(@Body() createProductDto: CreateProductDto) {
+        return this.productsService.createProduct(createProductDto);
       }
+
 }

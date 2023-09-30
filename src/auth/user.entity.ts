caf3ae53fc800819e8741b508080769cd33108f1
Nadim-Nahle <nahlenadim@gmail.com>
2023-09-30 12:51:17 +0300

    Create user entity

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  uid: string;
+
+  @Column()
+  email: string;
+
+  // Add more user properties as needed
+}

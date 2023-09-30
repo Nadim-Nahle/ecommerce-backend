9d81f11598d2bfc7ebe6b335a3b7bdb955bfea1f
Nadim-Nahle <nahlenadim@gmail.com>
2023-09-30 13:05:07 +0300

    update signup dto to add name

--- src/auth/signup.dto.ts
+++ src/auth/signup.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsString, Length } from 'class-validator';
 
 export class SignupDto {
-    @IsEmail()
-    email: string;
-  
-    @IsString()
-    @Length(6, 20)
-    password: string;
-  }
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @Length(6, 20)
+  password: string;
+
+  @IsString()
+  name: string
+}

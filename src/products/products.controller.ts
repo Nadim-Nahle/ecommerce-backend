560e3785cb838fa8465bcacbdf9bd76d0eaaeb3b
Nadim-Nahle <nahlenadim@gmail.com>
2023-10-10 14:58:07 +0300

    add updateProduct

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,22 +1,45 @@
-import { Controller, Get, Post, Body, Response } from '@nestjs/common';
+import { Controller, Get, Post, Body, Response, Put,Param, NotFoundException } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './create-product.dto';
+import { Product } from './product.interface';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
-    @Get()
-    async getAllProducts(@Response() response: any) {
-      const products = await this.productsService.getAllProducts();
-      const totalCount = products.length; // You can modify this to get the actual count
-      response.header('X-Total-Count', totalCount.toString()); // Set the X-Total-Count header
-     
-      return response.json(products)
-    };
-      @Post('add')
-      create(@Body() createProductDto: CreateProductDto) {
-        return this.productsService.createProduct(createProductDto);
-      }
+  @Get()
+  async getAllProducts(@Response() response: any) {
+    const products = await this.productsService.getAllProducts();
+    const totalCount = products.length; // You can modify this to get the actual count
+    response.header('X-Total-Count', totalCount.toString()); // Set the X-Total-Count header
 
+    return response.json(products)
+  };
+
+  @Post('add')
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.createProduct(createProductDto);
+  }
+
+  @Put(':id')
+  async updateProduct(
+    @Param('id') id: string,
+    @Body() updatedProductData: Partial<Product>,
+    @Response() response: any
+  ): Promise<any> {
+    const updatedProduct: any = await this.productsService.updateProduct(id, updatedProductData);
+
+    return response.json(updatedProduct);
+  }
+
+  @Get(':id')
+  async getProductById(@Param('id') id: string): Promise<Product> {
+    const product = await this.productsService.getProductById(id);
+    
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    
+    return product;
+  }
 }

5d458dd1a4f913fc3d303465184ca0538f72dd0f
Nadim-Nahle <nahlenadim@gmail.com>
2023-10-23 19:17:22 +0300

    Create order dto

---
+++ src/orders/create-order.dto.ts
@@ -0,0 +1,28 @@
+import { IsString, IsNumber, IsArray, IsNotEmpty, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateOrderDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'phone_number is required' })
+  phone_number: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ProductDTO)
+  products: ProductDTO[];
+  
+}
+
+export class ProductDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'name is required' })
+  product_name: string;
+
+  @IsNumber()
+  @IsNotEmpty({ message: 'price is required' })
+  product_price: number;
+
+  @IsNumber()
+  @IsNotEmpty({ message: 'quantity is required' })
+  quantity: number;
+}

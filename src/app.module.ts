c97b980d5c9345cf9f69eddc8f8c2c6b3c7005ea
Nadim-Nahle <nahlenadim@gmail.com>
2023-10-23 19:17:05 +0300

    add env to gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ProductsModule } from './products/products.module';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthValidationMiddleware } from './auth-validation/auth-validation.middleware';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, OrdersModule, OrdersModule],
   controllers: [AppController, AuthController],
   providers: [
     AppService,

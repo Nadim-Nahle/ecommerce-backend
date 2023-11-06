1ceb97ba99de3f80b26719c3e3b248cc5fac35fa
Nadim-Nahle <nahlenadim@gmail.com>
2023-11-06 18:05:06 +0200

    and query controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Response, Put,Param, NotFoundException, ValidationPipe, UsePipes, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Response, Put, Param, NotFoundException, ValidationPipe, UsePipes, Delete, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './create-product.dto';
 import { Product } from './product.interface';
@@ -6,15 +6,6 @@ import { Product } from './product.interface';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
-
-  @Get()
-  async getAllProducts(@Response() response: any) {
-    const products = await this.productsService.getAllProducts();
-    const totalCount = products.length; // You can modify this to get the actual count
-    response.header('X-Total-Count', totalCount.toString()); // Set the X-Total-Count header
-
-    return response.json(products)
-  };
 
   @Get('category')
   async getAllCategories(@Response() response: any) {
@@ -43,28 +34,68 @@ export class ProductsController {
   }
 
   @Delete(':id')
-async deleteProduct(@Param('id') id: string, @Response() response: any): Promise<any> {
-  try {
-    await this.productsService.deleteProduct(id);
-    return response.status(204).send(); // Product successfully deleted
-  } catch (error) {
-    if (error.message === `Product with ID ${id} not found`) {
-      return response.status(404).json({ message: `Product with ID ${id} not found` });
-    } else {
+  async deleteProduct(@Param('id') id: string, @Response() response: any): Promise<any> {
+    try {
+      await this.productsService.deleteProduct(id);
+      return response.status(204).send(); // Product successfully deleted
+    } catch (error) {
+      if (error.message === `Product with ID ${id} not found`) {
+        return response.status(404).json({ message: `Product with ID ${id} not found` });
+      } else {
+        return response.status(500).json({ message: 'Internal server error' });
+      }
+    }
+  }
+
+  @Get()
+  async filterProducts(
+    @Query('q') filter: string,
+    @Response() response: any,
+  ) {
+    try {
+      if (!filter) {
+        
+      const products = await this.productsService.getAllProducts();
+      const totalCount = products.length; // You can modify this to get the actual count
+      response.header('X-Total-Count', totalCount.toString()); // Set the X-Total-Count header
+      return response.json(products)
+
+      }
+
+
+
+      const value  = filter;
+
+      const filteredProducts = await this.productsService.filterProductsByField(value);
+
+      const totalCount = filteredProducts.length; // You can modify this to get the actual count
+      response.header('X-Total-Count', totalCount.toString()); // Set the X-Total-Count header
+
+      return response.json(filteredProducts);
+    } catch (error) {
+      // Handle any errors that may occur during filtering
       return response.status(500).json({ message: 'Internal server error' });
     }
   }
-}
-
 
   @Get(':id')
   async getProductById(@Param('id') id: string): Promise<Product> {
     const product = await this.productsService.getProductById(id);
-    
+
     if (!product) {
       throw new NotFoundException('Product not found');
     }
-    
+
     return product;
   }
+
+  // @Get()
+  // async getAllProducts(@Response() response: any) {
+  //   const products = await this.productsService.getAllProducts();
+  //   const totalCount = products.length; // You can modify this to get the actual count
+  //   response.header('X-Total-Count', totalCount.toString()); // Set the X-Total-Count header
+
+  //   return response.json(products)
+  // };
+
 }
